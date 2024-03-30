94472e5f5a9efb6840383411659d2598b9e0d718
shankdvara <shashank.hv@dvara.com>
2024-03-30 20:46:15 +0530

    removing extra file

--- txt_to_speech.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS 
-import os
-
-mytext = input("enter the text which needed to be converted as speech\n")
-speech = gTTS(text=mytext, lang="en", slow=False) 
-speech.save("output.mp3")
-os.system("start output.mp3") 
