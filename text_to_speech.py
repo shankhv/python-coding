5038ee8f24850d80f66af1d9ed6741684e6a24e3
shankdvara <shashank.hv@dvara.com>
2024-03-30 21:19:07 +0530

    required changes

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,7 +2,9 @@ from gtts import gTTS
 import os
 
 mytext = input("Enter the text which needs to be converted to speech:\n")
+if len(mytext)==0:
+    mytext="PLEASE ENTER THE TEXT WITHOUT ANY WORD CAN NOT CONVERT TO SPEECH"
 speech = gTTS(text=mytext, lang="en", slow=False) 
-speech.save("welcome.mp3")
+speech.save("output.mp3")
 
 os.system("start welcome.mp3")
